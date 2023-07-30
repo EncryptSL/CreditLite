0b4a5fb761ea79ac805f19b340a73bc8f86c6094
Lucyy_ <72966048+LcyDev@users.noreply.github.com>
2023-07-30 19:20:24 -0300

    Applied changes

--- src/main/kotlin/encryptsl/cekuj/net/api/economy/LiteEcoEconomyAPI.kt
+++ src/main/kotlin/encryptsl/cekuj/net/api/economy/LiteEcoEconomyAPI.kt
@@ -52,11 +52,7 @@ class LiteEcoEconomyAPI(private val liteEco: LiteEco) : LiteEconomyAPIProvider {
         return liteEco.preparedStatements.getTopBalance()
     }
 
-    override fun compacted(amount: Double): String {
-        return amount.moneyFormat(true)
-    }
-
     override fun formatting(amount: Double): String {
-        return amount.moneyFormat(liteEco.config.getString("plugin.economy.prefix").toString(), liteEco.config.getString("plugin.economy.name").toString(), liteEco.config.getBoolean("plugin.economy.compact_display"))
+        return amount.moneyFormat(liteEco.config.getString("economy.currency_prefix").toString(), liteEco.config.getString("economy.currency_name").toString())
     }
 }
