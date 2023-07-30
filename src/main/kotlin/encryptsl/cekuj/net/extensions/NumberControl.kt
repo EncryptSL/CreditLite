4bdd1af0055f8555715720e68c693279dcb9b241
Lucyy_ <72966048+LcyDev@users.noreply.github.com>
2023-07-30 19:51:15 -0300

    Remove unused extensions

--- src/main/kotlin/encryptsl/cekuj/net/extensions/NumberControl.kt
+++ src/main/kotlin/encryptsl/cekuj/net/extensions/NumberControl.kt
@@ -7,15 +7,3 @@ fun Double.isNegative(): Boolean {
 fun Double.isZero(): Boolean {
     return this == 0.0
 }
-
-fun Double.isApproachingZero(): Boolean {
-    return this < 0.01
-}
-
-fun String.isDecimal(): Boolean {
-    return toDoubleOrNull()?.takeIf { it.isFinite() } != null
-}
-
-fun String.toDecimal(): Double? {
-    return toDoubleOrNull()?.takeIf { it.isFinite() }
-}
