45f3693ac3b4434949131a5d5ea6437401f12a8c
EncryptSL <Rydlos123@gmail.com>
2024-05-10 01:21:54 +0200

    Push some changes...

--- src/main/java/com/github/encryptsl/credit/CreditLoader.java
+++ src/main/java/com/github/encryptsl/credit/CreditLoader.java
@@ -8,18 +8,55 @@ import org.eclipse.aether.graph.Dependency;
 import org.eclipse.aether.repository.RemoteRepository;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 @SuppressWarnings({"UnstableApiUsage", "unused"})
 public class CreditLoader implements PluginLoader {
+
     @Override
     public void classloader(@NotNull PluginClasspathBuilder pluginClasspathBuilder) {
         MavenLibraryResolver resolver = new MavenLibraryResolver();
 
-        resolver.addDependency(new Dependency(new DefaultArtifact("com.zaxxer:HikariCP:5.1.0"), null));
-        resolver.addDependency(new Dependency(new DefaultArtifact("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.23"), null));
-        resolver.addDependency(new Dependency(new DefaultArtifact("org.jetbrains.exposed:exposed-core:0.49.0"), null));
-        resolver.addDependency(new Dependency(new DefaultArtifact("org.jetbrains.exposed:exposed-jdbc:0.49.0"), null));
-        resolver.addDependency(new Dependency(new DefaultArtifact("org.jetbrains.exposed:exposed-kotlin-datetime:0.49.0"), null));
+        resolveLibraries().stream()
+                .map(DefaultArtifact::new)
+                .forEach(artifact -> resolver.addDependency(new Dependency(artifact, null)));
+        resolver.addRepository(new RemoteRepository.Builder(
+                "maven", "default", "https://repo.maven.apache.org/maven2/"
+        ).build());
 
         resolver.addRepository(new RemoteRepository.Builder("paper", "default", "https://repo.papermc.io/repository/maven-public/").build());
+
+        pluginClasspathBuilder.addLibrary(resolver);
+    }
+
+    private List<String> resolveLibraries() {
+        try {
+            return readLibraryListFromYaml();
+        } catch (IOException e) {
+            e.fillInStackTrace();
+        }
+        return new ArrayList<>();
+    }
+
+    private List<String> readLibraryListFromYaml() throws IOException {
+        List<String> libraries = new ArrayList<>();
+        InputStream inputStream = CreditLoader.class.getClassLoader()
+                .getResourceAsStream("paper-libraries.yml");
+
+        if (inputStream != null) {
+            try (inputStream; Scanner scanner = new Scanner(inputStream, StandardCharsets.UTF_8)) {
+                while (scanner.hasNextLine()) {
+                    libraries.add(scanner.nextLine());
+                }
+            }
+        } else {
+            System.err.println("paper-libraries.yml not found in the classpath.");
+        }
+        return libraries;
     }
 }
